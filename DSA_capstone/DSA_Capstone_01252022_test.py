ce398a2cf5e9d5881ec9a185ccf61aa8ae264bce
sue-c <sue-c@users.noreply.github.com>
2022-01-26 09:50:44 -0500

    Update DSA_Capstone_01252022_test.py
    Added import statsmodels.api as sm

--- DSA_capstone/DSA_Capstone_01252022_test.py
+++ DSA_capstone/DSA_Capstone_01252022_test.py
@@ -4,6 +4,7 @@ import pandas as pd
 import plotly.express as px
 import plotly.graph_objects as go
 from plotly.subplots import make_subplots
+import statsmodels.api as sm
 
 hwdata = pd.read_csv("DSA_capstone/data_capstone_dsa2021_2022.csv")
 
