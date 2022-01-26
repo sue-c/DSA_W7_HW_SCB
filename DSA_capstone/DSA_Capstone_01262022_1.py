e69bedb192af71e5356482332a13dfe050556412
sue-c <sue-c@users.noreply.github.com>
2022-01-26 15:18:20 -0500

    Update DSA_Capstone_01262022_1.py
    Sorted hwdata

--- DSA_capstone/DSA_Capstone_01262022_1.py
+++ DSA_capstone/DSA_Capstone_01262022_1.py
@@ -82,7 +82,7 @@ st.plotly_chart(fig9)
 
 st.subheader("Figure 7. Scatter plots between Rt Total and Sum Score buy Age")
 
-hwdata.sort_values(by=['age'], ascending=True)
+hwdata2 = hwdata.sort_values(by=['age'], ascending=True)
 
-fig10=px.scatter(hwdata, x="rt_total", y= "sum_score", trendline="ols", animation_frame="age")
+fig10=px.scatter(hwdata2, x="rt_total", y= "sum_score", trendline="ols", animation_frame="age")
 st.plotly_chart(fig10)
